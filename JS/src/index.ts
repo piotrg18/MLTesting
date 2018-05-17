63d958ddece8f7e751bb737f42b3cc2a2546065d
Piotr Grota <piotrgrota@gmail.com>
2018-05-17 20:18:39 +0200

    clean not used file

--- JS/src/index.ts
+++ JS/src/index.ts
@@ -1,7 +1,6 @@
 import {Player}  from './player';
 import {Obstacle} from "./obstacle"
 import {Game} from "./Game"
-import * as tf from '@tensorflow/tfjs';
 
 declare var p5: any;
 let player  :Player; 
@@ -15,43 +14,23 @@ var sketch = (p) => {
   p.setup = () => {
       p.createCanvas(640,480);
       p.frameRate(60);
-      
-      //console.log("Dummy");
+    
   };
 
   p.draw = () => {
       p.background(100);
       game.draw(p, player);
-      //player.show();
+    
   };
 
   p.keyPressed = () => {
     if (p.key == ' ') {
         player.move();
-        //console.log("SPACE");
+     
       }
   };
 };
 var sketchP = new p5(sketch);
-console.log("Dummy");
-
-    // Define a model for linear regression.
-const model = tf.sequential();
-model.add(tf.layers.dense({units: 1, inputShape: [1]}));
-
-// Prepare the model for training: Specify the loss and the optimizer.
-model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
-
-
-console.log("Data sdadas");
-// Generate some synthetic data for training.
-const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
-const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
-
-console.log("End");
-
-
-  
 
 
  
