c25fd9dd02c08a54db44fd1a91bf791866b55b94
Piotr Grota <piotrgrota@gmail.com>
2018-06-03 21:00:15 +0200

    init loaded images and json

--- game/src/index.ts
+++ game/src/index.ts
@@ -1,28 +1,42 @@
 import SpriteSheet from "./SpriteSheet";
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { Observable,of,animationFrameScheduler } from 'rxjs';
+import { Observable,of,animationFrameScheduler, forkJoin } from 'rxjs';
 import { Scheduler ,  } from "rxjs/Scheduler";
 import { fromEvent } from 'rxjs/observable/fromEvent';
-import { buffer, bufferCount,expand, filter, map,  share, tap, withLatestFrom } from 'rxjs/operators';
+import { buffer, bufferCount,expand, filter, map,  share, tap, withLatestFrom,timestamp} from 'rxjs/operators';
 import { interval } from "rxjs";
-
-
-
-
-var dom = {};
-
-of("1","2").pipe(share()).subscribe(function(d){
-    console.log(d);
-});
-
+import { fromPromise } from "rxjs/internal-compatibility";
+import {loadImage,loadLevel} from "./loaders";
 
 
 function fps(v) {
     return interval(1000 / v,animationFrameScheduler)  ;
 }
+var dom = {};
 
-fps(60).subscribe({
-    next:function(r){
-       console.log("dsad");
-    }
+let imageObservable =  fromPromise(loadImage("public/img/tiles.png"));
+let levelLoaders =  fromPromise(loadLevel("public/1-1.json"));
+forkJoin(imageObservable,levelLoaders).subscribe(function(results){
+    console.log(results);
+    
+});
+//withForkJoin()
+
+
+// fps(60).subscribe({
+//     next:function(r){
+//        console.log(r);
+//     }
+// });
+
+function fps2(v) {
+    return interval(1000 / v).pipe(
+      timestamp(),
+      bufferCount(2, 1)
+      ,map(function (w) { 
+          return w[1].timestamp - w[0].timestamp; })
+      ,share());
+}
+fps2(60).pipe(map(function (t) { return t / 20; })).subscribe(function(d){
+    console.log(d);
 });
